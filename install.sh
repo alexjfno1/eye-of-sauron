bcfd7adaa8925b78d0d8a997049e12c4a3c87d24
Alex Foster <alexjfno1@gmail.com>
2014-12-29 19:13:11 +0000

    Adding install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo -e "Usage: ./install.sh <SERVO_KEY>"
+  exit
+fi
+
+# Install Node
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+rm node_lastest_armhf.deb
+
+#Install Pi-Blaster
+git clone https://github.com/sarfata/pi-blaster.git
+cd pi-blaster
+sudo apt-get install autoconf
+./autogen.sh
+./configure
+make
+sudo make install
+cd ../
+rm -rf pi-blaster
+
+#Install dependencies
+npm install
+
+#Setup servo key
+sed s/SERVO_KEY/\$1/ pi-client.js
